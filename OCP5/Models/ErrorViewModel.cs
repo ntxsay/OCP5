a6b6b0316ba6dc35a0c26a26468add97b15c1d44
Loïc Bastaraud <39699697+ntxsay@users.noreply.github.com>
2025-05-12 11:15:33 -0400

    Ajoutez des fichiers projet.

---
+++ OCP5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OCP5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
