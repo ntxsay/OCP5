62a9c4d48ddc625d1788a12ea35b7f5f3f2723af
Loïc Bastaraud <39699697+ntxsay@users.noreply.github.com>
2025-05-22 10:04:05 -0400

    Ajout depot de referentiel générique et pour le véhicule

---
+++ OCP5/Services/IRepository.cs
@@ -0,0 +1,93 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using OCP5.Data;
+
+namespace OCP5.Services;
+
+public interface IRepository<T> where T : class
+{
+    public T? GetById(int id);
+    public Task<T?> GetByIdAsync(int id);
+    public IEnumerable<T> GetAll();
+    public Task<IEnumerable<T>> GetAllAsync();
+    public IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+    public void Add(T entity);
+    public Task AddAsync(T entity);
+    public void AddRange(IEnumerable<T> entities);
+    public Task AddRangeAsync(IEnumerable<T> entities);
+    public void Remove(T entity);
+    public void RemoveRange(IEnumerable<T> entities);
+    public void SaveChanges();
+    public Task SaveChangesAsync();
+}
+
+public class Repository<T>(ApplicationDbContext context) : IRepository<T>
+    where T : class
+{
+    protected readonly ApplicationDbContext Context = context;
+
+    public void Add(T entity)
+    {
+        Context.Set<T>().Add(entity);
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await Context.Set<T>().AddAsync(entity);
+    }
+
+    public void AddRange(IEnumerable<T> entities)
+    {
+        Context.Set<T>().AddRange(entities);
+    }
+
+    public async Task AddRangeAsync(IEnumerable<T> entities)
+    {
+        await Context.Set<T>().AddRangeAsync(entities);
+    }
+
+    public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+    {
+        return Context.Set<T>().Where(expression);
+    }
+
+    public virtual IEnumerable<T> GetAll()
+    {
+        return Context.Set<T>().ToList();
+    }
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await Context.Set<T>().ToListAsync();
+    }
+
+    public virtual T? GetById(int id)
+    {
+        return Context.Set<T>().Find(id);
+    }
+
+    public virtual async Task<T?> GetByIdAsync(int id)
+    {
+        return await Context.Set<T>().FindAsync(id);
+    }
+
+    public void Remove(T entity)
+    {
+        Context.Set<T>().Remove(entity);
+    }
+
+    public void RemoveRange(IEnumerable<T> entities)
+    {
+        Context.Set<T>().RemoveRange(entities);
+    }
+
+    public void SaveChanges()
+    {
+        Context.SaveChanges();
+    }
+    
+    public async Task SaveChangesAsync()
+    {
+        await Context.SaveChangesAsync();
+    }
+}
---
+++ OCP5/Services/IVehiclesRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OCP5.Data;
+using OCP5.Models.Entities;
+
+namespace OCP5.Services;
+
+public interface IVehiclesRepository : IRepository<Vehicle>;
+
+public class VehiclesRepository(ApplicationDbContext context) : Repository<Vehicle>(context), IVehiclesRepository
+{
+    public override async Task<IEnumerable<Vehicle>> GetAllAsync()
+    {
+        return await Context.Vehicles
+            .Include(v => v.Brand)
+            .Include(v => v.Finition)
+            .Include(v => v.Model)
+            .Include(v => v.VehicleYear).ToArrayAsync();
+    }
+    
+    public override IEnumerable<Vehicle> GetAll()
+    {
+        return Context.Vehicles
+            .Include(v => v.Brand)
+            .Include(v => v.Finition)
+            .Include(v => v.Model)
+            .Include(v => v.VehicleYear);
+    }
+    
+    public override async Task<Vehicle?> GetByIdAsync(int id)
+    {
+        return await Context.Vehicles
+            .Include(v => v.Brand)
+            .Include(v => v.Finition)
+            .Include(v => v.Model)
+            .Include(v => v.VehicleYear)
+            .SingleOrDefaultAsync(s => s.Id == id);
+    }
+    
+    public override Vehicle? GetById(int id)
+    {
+        return Context.Vehicles
+            .Include(v => v.Brand)
+            .Include(v => v.Finition)
+            .Include(v => v.Model)
+            .Include(v => v.VehicleYear)
+            .SingleOrDefault(s => s.Id == id);
+    }
+}
