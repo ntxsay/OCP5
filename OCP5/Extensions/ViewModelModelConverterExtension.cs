48f5380913c3ad97ee94aeb1779b5ebeae7837bf
Loïc Bastaraud <39699697+ntxsay@users.noreply.github.com>
2025-05-22 13:03:55 -0400

    Ajout du viewModel vehicules et extension permettant de convertir Modele et ViewModel et vice-versa

---
+++ OCP5/Extensions/ViewModelModelConverterExtension.cs
@@ -0,0 +1,35 @@
+﻿using OCP5.Models.Entities;
+using OCP5.Models.ViewModels;
+
+namespace OCP5.Extensions;
+
+public static class ViewModelModelConverterExtension
+{
+    public static Vehicle ConvertToModel(this VehicleViewModel self)
+    {
+        return new Vehicle()
+        {
+            Id = self.Id,
+            BrandId = self.BrandId,
+            ModelId = self.ModelId,
+            FinitionId = self.FinitionId,
+            VehicleYearId = self.VehicleYearId,
+            VinCode = null,
+            PurchasePrice = 0,
+            SellingPrice = self.SellingPrice,
+        };
+    }
+
+    public static  VehicleViewModel ConvertToViewModel(this Vehicle self)
+    {
+        return new VehicleViewModel()
+        {
+            Id = self.Id,
+            BrandId = self.BrandId,
+            ModelId = self.ModelId,
+            FinitionId = self.FinitionId,
+            VehicleYearId = self.VehicleYearId,
+            SellingPrice = self.SellingPrice,
+        };
+    }
+}
---
+++ OCP5/Models/ViewModels/VehicleViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace OCP5.Models.ViewModels;
+
+public class VehicleViewModel
+{
+    public int Id { get; set; }
+    
+    [DisplayName("Marque")]
+    [Required(ErrorMessage = "Veuillez sélectionner un marque.")]
+    public int BrandId { get; set; }
+    
+    [DisplayName("Modèle")]
+    [Required(ErrorMessage = "Veuillez sélectionner un modèle.")]
+    public int ModelId { get; set; }
+    
+    [DisplayName("Finition")]
+    [Required(ErrorMessage = "Veuillez sélectionner une finition.")]
+    public int FinitionId { get; set; }
+    
+    [DisplayName("Année")]
+    [Required (ErrorMessage = "Veuillez sélectionner une année.")]
+    public int VehicleYearId { get; set; }
+    
+    
+    [DisplayName("Prix de vente")]
+    [Required(ErrorMessage = "Veuillez saisir un prix de vente.")]
+    [DataType(DataType.Currency)]
+    [Range(0, double.MaxValue, ErrorMessage = "Veuillez saisir un prix de vente valide.")]
+    public double SellingPrice { get; set; }
+    
+    [Display(Name = "Visuel")]
+    public IFormFile? File { get; set; }
+
+    public SelectList? Brands { get; set; } = null!;
+    public SelectList? Models { get; set; } = null!;
+    public SelectList? Finitions { get; set; } = null!;
+    
+    public SelectList? VehicleYears { get; set; } = null!;
+}
