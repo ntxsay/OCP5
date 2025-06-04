84f55319aea16137392941755a2d9d219c55c827
Loïc Bastaraud <39699697+ntxsay@users.noreply.github.com>
2025-06-04 05:48:22 -0400

    Ajout des commentaires et amélioration du code

--- OCP5/Services/Repositories/IModelRepository.cs
+++ OCP5/Services/Repositories/IModelRepository.cs
@@ -7,8 +7,24 @@ namespace OCP5.Services.Repositories;
 
 public interface IModelRepository : IRepository<Model>
 {
+    /// <summary>
+    /// Récupère tous les modèles d'une marque spécifique.
+    /// </summary>
+    /// <param name="brandId"></param>
+    /// <returns></returns>
     Task<IEnumerable<Model>> GetAllAsync(int brandId);
+    
+    /// <summary>
+    /// Génère une liste de sélection de tous les modèles.
+    /// </summary>
+    /// <returns></returns>
     Task<SelectList> GetSelectListAsync();
+    
+    /// <summary>
+    /// Génère une liste de sélection des modèles d'une marque spécifique.
+    /// </summary>
+    /// <param name="brandId"></param>
+    /// <returns></returns>
     Task<SelectList> GetSelectListAsync(int brandId);
 }
 
@@ -16,7 +32,7 @@ public class ModelRepository(ApplicationDbContext context) : Repository<Model>(c
 {
     public virtual async Task<IEnumerable<Model>> GetAllAsync(int brandId)
     {
-        return await Context.Set<Model>().Where(m => m.IdBrand == brandId).ToArrayAsync();
+        return await Context.Models.Where(m => m.IdBrand == brandId).ToArrayAsync();
     }
     public async Task<SelectList> GetSelectListAsync(int brandId)
     {
